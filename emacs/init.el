bbf27855e5e7e4d873c4505675af9e0be828dd66
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2025-02-26 09:28:29 +0300

    emacs: add empv-hydra

--- emacs/init.el
+++ emacs/init.el
@@ -8123,9 +8123,12 @@ Useful if .elfeed directory is freshly syncned."
   :general
   (im-leader
     "r" empv-map
+    "R" #'empv-hydra/body
     "r4" #'empv-subsonic-search)
   :config
   (require 'im-radio-channels)
+  (require 'empv-hydra)
+
   (setq empv-radio-channels im-radio-channels)
   (setq empv-radio-log-file "~/Documents/notes/songs.org")
   (setq empv-base-directory "~/Music/")
